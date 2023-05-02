e812cafd21103666b267e83cc0bce88813c81472
Abdul Rehman <abdulrehmanmalikofficial2@gmail.com>
2023-05-02 17:57:28 +0500

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Counter</h1>
+      <h1>Counter in React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
